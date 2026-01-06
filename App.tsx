87d57cc7984fb9db41482d3619a0fd073c71560f
juanchoperezdel <perezdelgadojc@gmail.com>
2026-01-06 18:59:38 -0300

    Fix missing imports in App.tsx

--- App.tsx
+++ App.tsx
@@ -2,9 +2,19 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
 
-// ... (keep ScrollToTop and imports)
+// ScrollToTop Component to reset scroll on route change
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const App: React.FC = () => {
   return (
